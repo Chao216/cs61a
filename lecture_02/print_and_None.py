2c0d61cc52ebe42f63581b0b77969f4ce4af5fec
chao jiang <co.jiaan@proton.me>
2023-06-10 21:04:29 +0800

    this is a regular auto push

--- lecture_02/print_and_None.py
+++ lecture_02/print_and_None.py
@@ -5,3 +5,7 @@ def square(x):
 four = square(2)
 
 print(four)
+
+print(print(1997),print(2),print(16))
+
+# print is a non-pure function, it returns nothing, but display the argument on screen. It actually return None !
