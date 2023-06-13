265602d53ed49efba10613915d55284f00e39105
chao jiang <co.jiaan@proton.me>
2023-06-13 20:24:50 +0800

    this is a regular auto push

---
+++ lecture_03/doctring.py
@@ -0,0 +1,8 @@
+def square(x):
+    ''' This is a practice of docstring in function square
+    x is the value input which you want to square
+    simply rise to 2nd power
+    '''
+    return x ** 2
+
+print(help(square))
---
+++ lecture_03/environment.py
@@ -0,0 +1,10 @@
+from math import pi
+
+tau = 2 * pi
+print(tau)
+
+f = max
+
+max = 5
+
+print(f(10,12,1,234))
