b79ea1b9e9769102a642b2857a1530273985f331
chao jiang <co.jiaan@proton.me>
2023-06-17 23:50:50 +0800

    this is a regular auto push

---
+++ extra_/bit_operation.py
@@ -0,0 +1,13 @@
+"""bit operation is much fast than arithmetic operation"""
+
+a = 0
+b = 1
+print(f"a = : ",a)
+print(f"b = : ",b)
+print(f"a & b : ",a & b)
+print(f"a | b : ", a | b)
+print(f"a ^ b : ", a ^ b)
+print(f"~ a = ; ", ~a)
+print(f"~ b = ; ", ~b)
+print(f"a >> 5 : ", a >> 5)
+print(f"b << 15 : ", b << 15)
