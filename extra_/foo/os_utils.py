a82f18b393fa69a7e22173d3b22a52dea2f59300
chao <co.jiaan@proton.me>
2023-08-31 00:28:14 +0800

    this is a regular auto push

---
+++ extra_/foo/os_utils.py
@@ -0,0 +1,13 @@
+import os
+
+print(os.getcwd())
+
+print(os.listdir())
+
+print(os.mkdir("folder created by python os module"))
+
+print(os.listdir())
+
+print(os.rmdir("folder created by python os module"))
+
+print(os.listdir())
