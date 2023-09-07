e66fb0eb31b57a50d22f5df3a2f3ec9d6de79e2e
chao <co.jiaan@proton.me>
2023-09-07 23:38:12 +0800

    this is a regular auto push

--- lecture_06/ex.py
+++ lecture_06/ex.py
@@ -8,6 +8,8 @@ def addone(x):
 def square(x):
 	return x * x
 
+
+
 def transform(n, term):
 	res = term(n)
 	return res
@@ -18,3 +20,20 @@ print(ans1)
 
 ans2 = transform(7,square)
 print(ans2)
+
+# now return a function
+
+def make_function(n):
+	def add_some(x):
+		return x + n
+	return add_some
+
+add_5 = make_function(5)
+
+ans3 = add_5(10)
+print(ans3)
+
+minus_7 = make_function(-7)
+
+ans4 = minus_7(1000)
+print(ans4)
