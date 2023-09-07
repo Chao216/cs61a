f9faaafdb460cab4cb44b45a76c31ce45f1ac3f8
chao <co.jiaan@proton.me>
2023-09-07 23:33:56 +0800

    this is a regular auto push

--- lecture_06/ex.py
+++ lecture_06/ex.py
@@ -5,8 +5,16 @@
 def addone(x):
 	return x + 1
 
+def square(x):
+	return x * x
+
 def transform(n, term):
 	res = term(n)
 	return res
+
+
 ans1 = transform(10,addone)
 print(ans1)
+
+ans2 = transform(7,square)
+print(ans2)
