56341ee9bb24c21dd4d4ed3dc6de3436cec65ac6
chao <co.jiaan@proton.me>
2023-09-11 23:59:45 +0800

    this is a regular auto push

--- lecture_07/env_lambda.py
+++ lecture_07/env_lambda.py
@@ -16,3 +16,25 @@ def outter(a_func):
     return lambda z: a_func(z) * apple
 
 print(outter(lambda x: x + 100)(3)) # here lambda x : x + 100 is "a_func", 3 is z in return lambda expression, apple is 7
+
+tee = 10
+
+def outter(a_func):
+    tee = 9
+    def inner(a_number):
+        return a_func(a_number) * tee
+    return inner
+
+def cube(n):
+    return n * n * n
+
+print(outter(cube)(3))
+
+assert outter(cube)(3) == 243
+
+print("first assertion passed!")
+
+# recap , what does print(return)? None
+
+print(693)
+print(print(693))
