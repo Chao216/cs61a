b516d1869408280b92db28dbd3cb3515524254d7
chao <co.jiaan@proton.me>
2023-09-12 00:10:33 +0800

    this is a regular auto push

--- lecture_07/env_lambda.py
+++ lecture_07/env_lambda.py
@@ -2,18 +2,18 @@ a = 1
 
 def f(g):
     a = 2
-    return lambda y: g(y) * a
+    return lambda y: g(y) * a # here a is 2
 
-print(f(lambda x: a + x)(a))
+print(f(lambda x: a + x)(a))# first a in lambda is global (1) ==> lambda x : 1 + x, second a also global , we get (1 + 1) * 2 == 4
 
-print(f(lambda x: x* x)(3))
+print(f(lambda x: x* x)(3)) # (3 * 3) * 2 == 18.
 
 
 apple = 5
 
 def outter(a_func):
     apple = 7
-    return lambda z: a_func(z) * apple
+    return lambda z: a_func(z) * apple # here the apple is a local variable inside code block
 
 print(outter(lambda x: x + 100)(3)) # here lambda x : x + 100 is "a_func", 3 is z in return lambda expression, apple is 7
 
