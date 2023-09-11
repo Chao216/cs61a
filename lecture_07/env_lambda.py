8f53e38cd829da91baaf2d09065c582c416b1fb6
chao <co.jiaan@proton.me>
2023-09-11 23:53:39 +0800

    this is a regular auto push

--- lecture_07/env_lambda.py
+++ lecture_07/env_lambda.py
@@ -4,4 +4,15 @@ def f(g):
     a = 2
     return lambda y: g(y) * a
 
-f(lambda x: a + x)(a)
+print(f(lambda x: a + x)(a))
+
+print(f(lambda x: x* x)(3))
+
+
+apple = 5
+
+def outter(a_func):
+    apple = 7
+    return lambda z: a_func(z) * apple
+
+print(outter(lambda x: x + 100)(3)) # here lambda x : x + 100 is "a_func", 3 is z in return lambda expression, apple is 7
