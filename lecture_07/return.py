cc3ea2b417252fb32670c5f4dacfce71af0941eb
chao <co.jiaan@proton.me>
2023-09-12 23:41:33 +0800

    this is a regular auto push

--- lecture_07/return.py
+++ lecture_07/return.py
@@ -17,3 +17,14 @@ def bigger_than_ten(x):
     return x > 10 # here true or false will be returned
 
 print(search(bigger_than_ten))
+
+def cube(x):
+    return x * x * x
+
+def inverse(a_func): # pass in a function to be used later
+    # we are looking for a function that do reverse job
+    return lambda z: search(lambda x :a_func(x) == z)
+
+reverse_cube = inverse(cube)
+print(reverse_cube(27))
+print(reverse_cube(8))
