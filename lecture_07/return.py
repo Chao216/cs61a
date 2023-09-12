c4e0ae93c8cee70b8cd6e6ed043239de1572d906
chao <co.jiaan@proton.me>
2023-09-12 23:33:46 +0800

    this is a regular auto push

--- lecture_07/return.py
+++ lecture_07/return.py
@@ -5,3 +5,15 @@ def demo():
     print("senetnce after return")
 
 demo()
+
+def search(a_func):
+    x = 0
+    while not a_func(x): # when a_func(x) is not true
+        x += 1
+    # will liip until condition is true
+    return x
+
+def bigger_than_ten(x):
+    return x > 10 # here true or false will be returned
+
+print(search(bigger_than_ten))
