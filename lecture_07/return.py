607a001db3a783b306e28d8b67d3cca2d838bf22
chao <co.jiaan@proton.me>
2023-09-13 00:04:08 +0800

    this is a regular auto push

--- lecture_07/return.py
+++ lecture_07/return.py
@@ -25,6 +25,40 @@ def inverse(a_func): # pass in a function to be used later
     # we are looking for a function that do reverse job
     return lambda z: search(lambda x :a_func(x) == z)
 
+# a_func(x) == z outcome is either true or false
+# search function will loop x +1 until true is satisified
+# z ===>  x, x will increase until conditiion is meet
+
 reverse_cube = inverse(cube)
 print(reverse_cube(27))
 print(reverse_cube(8))
+
+def reverse(origin_func): # a function to reverse
+    def mirror_func(num): # the wanted function, take a num arg
+        return search((lambda c : origin_func(c)==num)) # return c
+        ##########################################################
+        # suppose origin func is square
+        # search(lambda c : square(c)== num )
+        # c starts from 0
+        # if square(c) != num, we get false
+        # loop in search continue
+        # try square(1)... square(2)...square(3)......
+        # until find correct c, and get c returned
+
+    return mirror_func
+    # as high order function, the mirror function is then returned
+
+
+## thin of origin_func(x) == z
+## now we need mirror_func(z) == x
+def plusfive(n):
+    return n + 5
+
+revPlusFive = reverse(plusfive)
+print(revPlusFive(100))
+
+def mulnine(n):
+    return n * 9
+
+test = reverse(mulnine)
+print(test(63))
