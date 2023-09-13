e428d617c50a7b9d414aea3ef80fecc5efc0fb51
chao <co.jiaan@proton.me>
2023-09-14 00:14:36 +0800

    this is a regular auto push

--- lecture_07/logical_op.py
+++ lecture_07/logical_op.py
@@ -8,6 +8,8 @@ def has_big_sqrt(n):
 for i in range(1,101):
     print(has_big_sqrt(i))
 
+# OR
+
 def by_3_or_by_5(n):
     return n % 3 == 0 or n % 5 == 0
 
@@ -15,3 +17,20 @@ print("*****************************************")
 
 for i in range(1,101):
     print("i = ", i, "\t",by_3_or_by_5(i))
+
+
+# NOT
+
+print("10 != 100", "\t",10 != 100)
+
+# XOR
+
+# cond 1 must be different from cond 2
+# review bitwise operation
+a = True
+b = False
+c = True
+
+print(a ^ b)
+print(b ^ c)
+print(a ^ c)
