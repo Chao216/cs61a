30c8657810d11931c6ae8e19a0ea43193e6934de
chao <co.jiaan@proton.me>
2023-09-14 00:09:15 +0800

    this is a regular auto push

--- lecture_07/logical_op.py
+++ lecture_07/logical_op.py
@@ -7,3 +7,11 @@ def has_big_sqrt(n):
 
 for i in range(1,101):
     print(has_big_sqrt(i))
+
+def by_3_or_by_5(n):
+    return n % 3 == 0 or n % 5 == 0
+
+print("*****************************************")
+
+for i in range(1,101):
+    print("i = ", i, "\t",by_3_or_by_5(i))
