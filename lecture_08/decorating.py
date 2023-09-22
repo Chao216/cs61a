fbd117fd6e51f0eec092e38b7a80357457350a34
chao <co.jiaan@proton.me>
2023-09-22 23:44:57 +0800

    this is a regular auto push

--- lecture_08/decorating.py
+++ lecture_08/decorating.py
@@ -11,3 +11,43 @@ def square(n):
     return n * n
 
 square(100)
+
+def decorator2(func2):
+    print("decorator2 triggered !")
+    print()
+    print(func2)
+    print()
+    def wrappred(number):
+        return func2(number)
+    return wrappred
+    print("hello, I am decorator 2 again")
+
+@decorator2
+def plusfive(n):
+    return n + 5
+
+plusfive(10)
+
+def whichfunc(a_func):
+    print(a_func," is being called!")
+    def inner(arg1):
+        return a_func
+    return inner
+
+@whichfunc
+def f1(a):
+    @whichfunc
+    def f2(b):
+        @whichfunc
+        def f3(c):
+            @whichfunc
+            def f4(d):
+                @whichfunc
+                def f5(e):
+                    return a + b + c + d + e
+                return f5
+            return f4
+        return f3
+    return f2
+
+f1(1)(2)(3)(4)(5)
