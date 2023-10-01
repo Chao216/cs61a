2e44b050dc42b2e388dca10a41139ae287a21ba1
co.jiaan <co.jiaan@proton.me>
2023-10-01 11:47:05 -0700

    this is a regular auto push

--- lecture_11/list.py
+++ lecture_11/list.py
@@ -3,3 +3,9 @@ digits=[0,1,2,3,4,5,6,7,8,9,10]
 print(digits[8])
 
 print(len(digits))
+
+# you can add, multiply a list
+
+from operator import add, mul
+list1 = add([67,12,32,4232,],mul(digits,2))
+print(list1)
