abdc2a715e21d79dd9d90b7b98a05ce33857295b
co.jiaan <co.jiaan@proton.me>
2023-10-02 09:51:24 -0700

    this is a regular auto push

---
+++ lecture_12/dict.py
@@ -0,0 +1,22 @@
+dic1 = {"chao":26,"jiaxing":26,"yulei":27}
+
+print(dic1)
+
+# remember to iterate through, don't foregt ()
+for i in dic1.items():
+    print(i)
+
+for i in dic1.keys():
+    print(i)
+
+for i in dic1.values():
+    print(i)
+
+for i, j in zip(dic1.keys(),dic1.values()):
+    print(i, " = ", j)
+
+l1 = ['chao','lei','jiaxing']
+l2 = ['coffee','coke','tea']
+
+for i,j in zip(l1,l2):
+    print(i," enjoys drink ", j)
---
+++ lecture_12/string_.py
@@ -0,0 +1,12 @@
+'hello'
+
+birth_place = "Zhejiang Jiaxing"
+
+birth_place_list = [*birth_place]
+print(birth_place_list)
+
+for i in birth_place:
+    print(i)
+
+for i in birth_place_list:
+    print(i)
