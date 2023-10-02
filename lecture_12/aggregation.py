4a1d8d47c50399fc89be75ce2126fd3dc864fe98
co.jiaan <co.jiaan@proton.me>
2023-10-02 09:35:04 -0700

    this is a regular auto push

--- lecture_12/aggregation.py
+++ lecture_12/aggregation.py
@@ -5,3 +5,19 @@ print(sum(list1))
 print(sum([2,3,4],5))
 
 print(sum([[1,2,3],[4,5,6]],[]))
+
+print(max(list1))
+print(max(1,2,3,2,1,4,66))
+
+# use key= function
+
+print(max(range(5,11),key=lambda x:-x)) # find which one return biggest value
+
+# all
+
+print(all(range(10)))
+
+print(all(range(1,10)))
+print(all(range(-10,10)))
+
+print(all(list1))
