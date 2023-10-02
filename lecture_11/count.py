572630e55b5bd2bf5dda65f3c9b793011343a8c0
co.jiaan <co.jiaan@proton.me>
2023-10-01 20:39:56 -0700

    this is a regular auto push

--- lecture_11/count.py
+++ lecture_11/count.py
@@ -12,3 +12,11 @@ count([1,1,1,2,2,2,3,1,1,1,1,1,3,2,1,2,3,2,1],1)
 
 
 
+def new_count(list_,n):
+    count = 0
+    for i in list_:
+        if i == n:
+            count += 1
+    print("we found ", count, " ",n, " in the list")
+    return count
+new_count([2,3,2,3,2,3,4,3,2,3,5,4,6,43,2],3)
