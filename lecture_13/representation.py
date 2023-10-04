5fc812ad5bb65c8c13e320aee900f2b11d4a7585
chao <co.jiaan@proton.me>
2023-10-04 18:58:58 +0800

    this is a regular auto push

--- lecture_13/representation.py
+++ lecture_13/representation.py
@@ -43,3 +43,10 @@ def div_rational(x,y):
     return rational(up,lo)
 
 print(div_rational(rational(1,3),rational(1,6)))
+
+def eq_ratioal(x,y):
+    return rational(x) == rational(y)
+
+r1 = rational(1,3)
+r2 = rational(2,6)
+print(eq_ratioal(r1,r2))
