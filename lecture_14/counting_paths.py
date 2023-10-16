8897112a51cb31507ccd064c43a6d1a32149660f
co.jiaan <co.jiaan@proton.me>
2023-10-16 10:09:41 +0800

    counting paths

--- lecture_14/counting_paths.py
+++ lecture_14/counting_paths.py
@@ -10,7 +10,21 @@ mytree = tree(3,[
         tree(8,[tree(2),tree(5)]),
         tree(1,[tree(2),tree(3)])
     ]),
-    tree(1)
+    tree(1,[
+        tree(7,[tree(3),tree(6)]),
+        tree(11,[tree(7),tree(3)])
+    ])
 ])
 
 print_tree(mytree)
+
+def count_paths(t,total):
+    if lable(t) == total:
+        found =1
+    else:
+        found = 0
+
+    return found + sum([count_paths(b,total) for b in branches(t)])
+
+for i in range(11):
+    print("total = ",i,"\tand we have found ",count_paths(mytree,i),"\tpaths")
