c390d1ea01228ec25a17ba4ca0266d511c4c4408
co.jiaan <co.jiaan@proton.me>
2023-10-23 22:57:02 +0800

    identity --> object |  equality ----> value

--- lecture_15/mutation.py
+++ lecture_15/mutation.py
@@ -7,3 +7,40 @@ b.append(30)
 print(a)
 
 print(b==a)
+
+c = [10]
+d = [10]
+
+print(c==d)
+
+print(c is d)
+
+print( a is b)
+
+# identity
+# exp0 is exp1
+# if both exp0 and exp1 evaluate to the same object
+
+# equality
+# exp0 == exp1
+# if both exp0 exp1 evaluates to the same value
+
+# identity ---> Same object
+# equality ---> same value
+
+tim = 26
+larry = tim
+
+print(larry is tim)
+print(larry == tim)
+
+myname = 'chao'
+yourname = 'chao'
+print(myname == yourname)
+print(myname is yourname)
+
+myage = 26
+yourage = 26
+
+print(myage == yourage)
+print(myage is yourage)
